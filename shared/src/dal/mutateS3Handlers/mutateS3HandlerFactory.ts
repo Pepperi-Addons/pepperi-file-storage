55317e5fb510fd640d0d40595adc04da304bcfb7
Saar Guttman <saar.g@pepperi.com>
2023-02-22 10:42:02 +0200

    Handle Hidden=true

--- shared/src/dal/mutateS3Handlers/mutateS3HandlerFactory.ts
+++ shared/src/dal/mutateS3Handlers/mutateS3HandlerFactory.ts
@@ -2,10 +2,11 @@ import { AbstractS3PfsDal } from "../abstractS3PfsDal";
 import { AMutateS3Handler } from "./aMutateS3Handler";
 import { MutateS3HandleExpiredFile } from "./mutateS3HandleExpiredFile";
 import { MutateS3HandleFileUpload } from "./mutateS3HandleFileUpload";
+import { MutateS3HandlePostBase } from "./mutateS3HandlePostBase";
 import { MutateS3HandlePresignedUrl } from "./mutateS3HandlePresignedUrl";
 import { MutateS3HandleTempFile } from "./mutateS3HandleTempFile";
 
-export type MutateS3HandleType = "tempFile" | "fileUpload" | "presignedUrl" | "expiredFile";
+export type MutateS3HandleType = "" | "tempFile" | "fileUpload" | "presignedUrl" | "expiredFile";
 export class MutateS3HandlerFactory
 {
     public static getHandler(
@@ -25,7 +26,7 @@ export class MutateS3HandlerFactory
             case "expiredFile":
                 return new MutateS3HandleExpiredFile(newFileFields, existingFile, s3PfsDal);
             default:
-                throw new Error(`Unknown type: ${type}`);
+                return new MutateS3HandlePostBase(newFileFields, existingFile, s3PfsDal);
         }
     }
 }
