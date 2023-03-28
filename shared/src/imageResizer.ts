483b587a2dba7a8350392c105dd7216e774f89f8
Saar Guttman <saar.g@pepperi.com>
2023-03-28 10:57:22 +0300

    Jimp refactor

--- shared/src/imageResizer.ts
+++ shared/src/imageResizer.ts
@@ -1,24 +1,34 @@
-import Jimp from "jimp";
+import configure from "@jimp/custom";
+import bmp from '@jimp/bmp';
+import jpeg from '@jimp/jpeg';
+import png from '@jimp/png';
+import tiff from '@jimp/tiff';
+
+import resize from "@jimp/plugin-resize";
 
 export class ImageResizer 
-{
-	constructor(private readonly mimeType,
+{	
+	protected supportedMimeTypes;
+	protected jimp: any;
+	constructor(private readonly mimeType: string,
         private readonly imageBuffer: Buffer) 
 	{
 
-		const supportedMimeTypes = [Jimp.MIME_BMP,
-			Jimp.MIME_JPEG,
-			Jimp.MIME_PNG,
-			Jimp.MIME_TIFF]
+		this.supportedMimeTypes = [bmp, jpeg, png, tiff];
 
-		if (!supportedMimeTypes.includes(mimeType)) 
+		if (!this.supportedMimeTypes.includes(mimeType)) 
 		{
 			throw new Error(`Bad Request. Creating a thumbnail for MIME type ${mimeType} is not supported.`)
 		}
+
+		this.jimp = configure({
+			types: this.supportedMimeTypes,
+			plugins: [resize],
+		  });
 	}
 
 	/**
-     * Resizes the image given in the constructor to be eqaul to or less than the requested scales.
+     * Resizes the image given in the constructor to be equal to or less than the requested scales.
      * Images smaller than the requested scales will not be enlarged. The Image cover is fit, meaning aspect ration will be preserved, 
      * and image will be in requested scale by cropping it.
      * @param requestedScales  the requested scales
@@ -28,7 +38,7 @@ export class ImageResizer
 	{
 		const requestedWidth = parseInt(requestedScales.Size.toLowerCase().split('x')[0]);
 		const requestedHeight = parseInt(requestedScales.Size.toLowerCase().split('x')[1]);
-		const buffer =  await (await Jimp.read(this.imageBuffer)).resize(requestedWidth, requestedHeight).getBufferAsync(this.mimeType);
+		const buffer =  await (await this.jimp.read(this.imageBuffer)).resize(requestedWidth, requestedHeight).getBufferAsync(this.mimeType);
 		return buffer
 	}
 
