07d28c05c469a7091e5e29a731634068c5ec2714
Saar Guttman <saar.g@pepperi.com>
2023-07-24 17:48:34 +0300

    Check IsSupportAdmin only if user is admin

--- server-side/PfsCommands/TransactionalCommands/postCommand/onlinePostService.ts
+++ server-side/PfsCommands/TransactionalCommands/postCommand/onlinePostService.ts
@@ -4,10 +4,19 @@ import jwtDecode from "jwt-decode";
 
 export class OnlinePostService extends PostService
 {
+	protected readonly adminEmployeeTypes: number[] = [1];
+	
 	protected async getUploadedByUUID(): Promise<any> 
 	{
 		const userId = jwtDecode(this.OAuthAccessToken)["pepperi.id"];
-		const isSupportAdminUser: boolean = (await this.papiClient.get(`/users/${userId}?fields=IsSupportAdminUser`)).IsSupportAdminUser;
+		const employeeType = jwtDecode(this.OAuthAccessToken)["pepperi.employeetype"];
+
+		let isSupportAdminUser = false;
+
+		if(this.adminEmployeeTypes.includes(employeeType))
+		{
+			isSupportAdminUser = (await this.papiClient.get(`/users/${userId}?fields=IsSupportAdminUser`)).IsSupportAdminUser;
+		}
 
 		//Leave files uploaded by support admin user (i.e. uploading using integration) with a blank 
 		return isSupportAdminUser ? "" : jwtDecode(this.OAuthAccessToken)["pepperi.useruuid"];
