152d469534b6a793aed5634883f5b92ccfe71915
Saar Guttman <saar.g@pepperi.com>
2023-08-24 15:03:06 +0300

    Stop relying on request in mutateS3Handlers

--- shared/src/dal/mutateS3Handlers/mutateS3HandleTempFile.ts
+++ shared/src/dal/mutateS3Handlers/mutateS3HandleTempFile.ts
@@ -6,7 +6,7 @@ export class MutateS3HandleTempFile extends MutateS3HandleFileCopy
 	{
 		// Copy the file's data from the temp location to the final location.
 		const absolutePath = this.s3PfsDal.relativeAbsoluteKeyService.getAbsolutePath(this.newFileFields.Key);
-		const res = await this.s3PfsDal.awsDal.copyS3Object(this.s3PfsDal.request.body.TemporaryFileURLs, absolutePath, this.shouldUseCache);
+		const res = await this.s3PfsDal.awsDal.copyS3Object(this.newFileFields.TemporaryFileURLs[0], absolutePath, this.shouldUseCache);
 
 		// Set the file version and size
 		this.newFileFields.FileVersion = res.$response.data?.VersionId;
