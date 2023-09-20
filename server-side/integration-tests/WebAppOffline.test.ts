0eaf2cea028cd17ba68ea6e5f850ec40b592a150
Saar Guttman <saar.g@pepperi.com>
2023-09-20 13:33:57 +0300

    Added a WebApp data URI POST integration test

--- server-side/integration-tests/WebAppOffline.test.ts
+++ server-side/integration-tests/WebAppOffline.test.ts
@@ -9,6 +9,39 @@ import { testFileData } from "./constants";
 export class WebAppOfflineTests extends APostOfflineTests
 {
 	title = "WebApp Offline tests";
+
+	override tests(describe: (suiteTitle: string, func: () => void) => void,
+		it: (name: string, fn: Mocha.Func) => void,
+		expect: Chai.ExpectStatic,
+	): void 
+	{
+		super.tests(describe, it, expect);
+
+		describe(this.title, () => 
+		{
+			it("Post a file offline with data URI and sync it ", async () => 
+			{
+				
+				// POST a file offline
+				const file: AddonFile = {
+					Key: `dataUri_${this.offlineTestFileName}`,
+					MIME: "image/png",
+					Cache: false,
+					Sync: "Always",
+					URI: testFileData,
+				};
+
+				// Post it offline
+				await this.pfsOfflineService.post(this.pfsSchemaName, file);
+
+				// Sync it
+				await this.sync(expect);
+
+				// Make sure it is synced
+				this.validateOnlineFile(expect, file.Key!);
+			});
+		});
+	}
 	
 	protected getExpectedOfflineUrlRegex(): RegExp
 	{
