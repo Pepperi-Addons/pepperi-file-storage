0ced1c423fef4749206e9a851582f4cec08758b7
Saar Guttman <saar.g@pepperi.com>
2023-12-24 15:37:27 +0200

    Add Open Sync endpoints (not implemented)

---
+++ server-side/sync_source.ts
@@ -0,0 +1,11 @@
+import { Client, Request } from "@pepperi-addons/debug-server/dist";
+
+export async function get_delta(client: Client, request: Request): Promise<any> 
+{
+	throw new Error('Not implemented');
+}
+
+export async function rebuild_cache(client: Client, request: Request): Promise<any> 
+{
+	throw new Error('Not implemented');
+}
