e61de8e2bb5e52b3a5b7f7fe567c1e34edef431b
Saar Guttman <saar.g@pepperi.com>
2024-01-03 16:54:46 +0200

    Crawler Source implementation

---
+++ server-side/sync-source/crawling-source/crawling-source.service.ts
@@ -0,0 +1,121 @@
+import { Request } from "@pepperi-addons/debug-server/dist";
+import { SearchData, AddonData, SearchBody } from "@pepperi-addons/papi-sdk";
+
+import docDbDal from "../../DAL/docDbDal";
+
+
+export interface CrawlerPageKey {
+    SchemaIndex: number;
+    SpecificSchemaPageKey?: string;
+}
+
+
+export class CrawlingSourceService
+{
+    constructor(protected docDbDal: docDbDal, protected request: Request)
+    {}
+
+    /**
+     * Get the next page of data to crawl.
+     * @returns {Promise<SearchData<AddonData>>} The next page of data to crawl.
+     */
+    public async getNextPage(): Promise<SearchData<AddonData>>
+    {
+        const pageKey: CrawlerPageKey = this.parsePageKey();
+        const searchBody: SearchBody = {
+            PageKey: pageKey.SpecificSchemaPageKey,
+            Fields: ["Key", "Hidden", "ModificationDateTime"],
+            IncludeDeleted: true,
+        }
+
+        const schemaName = this.request.body.SchemaNames[pageKey.SchemaIndex];
+        const result = await this.docDbDal.searchDataInTable(schemaName, searchBody);
+
+        // Since we can't pass to the crawl target the actual schema from which the data was taken,
+        // a transformation of the objects is required.
+        this.formatResult(result, schemaName, pageKey);
+
+        return result;
+    }
+
+    /**
+     * Format the result to be passed to the crawl target.
+     * @param {SearchData<AddonData>} result - The result to format.
+     * @param {string} schemaName - The name of the schema from which the data was taken.
+     * @param {CrawlerPageKey} currentPageKey - The page key of the current crawler page.
+     */
+    protected formatResult(result: SearchData<AddonData>, schemaName: string, currentPageKey: CrawlerPageKey): void
+    {
+        this.setOriginSchemaOnObjects(result, schemaName);
+        this.setNextPageKey(result, currentPageKey);
+    }
+
+    /**
+     * Set the origin schema name on the objects in the result.
+     * @param {SearchData<AddonData>} result - The result to set the origin schema name on.
+     * @param {string} schemaName - The name of the schema from which the data was taken.
+     */
+    protected setOriginSchemaOnObjects(result: SearchData<AddonData>, schemaName: string) {
+        result.Objects.forEach(object => {
+            object["ObjectSourceSchema"] = schemaName;
+        });
+    }
+
+    /**
+     * Set the next page key on the result.
+     * @param {SearchData<AddonData>} result - The result to set the next page key on.
+     * @param {CrawlerPageKey} currentCrawlerPageKey - The page key of the current crawler page.
+     */
+    protected setNextPageKey(result: SearchData<AddonData>, currentCrawlerPageKey: CrawlerPageKey): void
+    {
+        const nextPageKey: CrawlerPageKey = {
+            SchemaIndex: currentCrawlerPageKey.SchemaIndex,
+            SpecificSchemaPageKey: "",
+        };
+
+        // If there is a next page key, on the same schema, use it.
+        if(result.NextPageKey)
+        {
+            nextPageKey.SpecificSchemaPageKey = result.NextPageKey;
+            result.NextPageKey = JSON.stringify(nextPageKey);
+
+        }
+        // If there is no next page key, and there are more schemas to crawl, use the next schema.
+        else if(currentCrawlerPageKey.SchemaIndex + 1 < this.request.body.SchemaNames.length)
+        {
+            nextPageKey.SchemaIndex++;
+            result.NextPageKey = JSON.stringify(nextPageKey);
+        }
+        // If there is no next page key, and there are no more schemas to crawl, use an empty string
+        // to indicate that there are no more pages.
+        else
+        {
+            result.NextPageKey = ""
+        }
+    }
+
+    /**
+     * Parse the page key from the request.
+     * @returns {CrawlerPageKey} The parsed crawler page key.
+     */
+    protected parsePageKey(): CrawlerPageKey
+    {
+        const result: CrawlerPageKey = { SchemaIndex: 0, SpecificSchemaPageKey: "" };
+
+        const passedPageKey: string = this.request.body.PageKey;
+        if(passedPageKey)
+        {
+            const parsedPageKey: CrawlerPageKey = JSON.parse(passedPageKey);
+
+            result.SchemaIndex = parsedPageKey.SchemaIndex;
+            result.SpecificSchemaPageKey = parsedPageKey.SpecificSchemaPageKey ?? "";
+
+            if(!result.SchemaIndex)
+            {
+                throw new Error("Error parsing PageKey: SchemaIndex is required");
+            }
+        }
+
+        return result;
+    }
+}
--- server-side/sync_source.ts
+++ server-side/sync_source.ts
@@ -1,5 +1,5 @@
 import { Client, Request } from "@pepperi-addons/debug-server/dist";
-import { AddonAPIAsyncResult } from "@pepperi-addons/papi-sdk";
+import { AddonAPIAsyncResult, AddonData, SearchData } from "@pepperi-addons/papi-sdk";
 
 import { AddonUUID as PfsAddonUUID } from "../addon.config.json";
 import { SyncSourceService } from "./sync-source/sync-source.service";
@@ -7,6 +7,8 @@ import { ICrawlRequest } from "./sync-source/rebuild-cache/i-crawl-request";
 import { CrawlRequest } from "./sync-source/rebuild-cache/crawl-request";
 import { ServerHelper } from "./serverHelper";
 import { SchemaSearcher } from "./sync-source/rebuild-cache/schema-searcher";
+import docDbDal from "./DAL/docDbDal";
+import { CrawlingSourceService } from "./sync-source/crawling-source/crawling-source.service";
 
 export async function rebuild_cache(client: Client, request: Request): Promise<AddonAPIAsyncResult> 
 {
@@ -24,9 +26,12 @@ export async function update_cache(client: Client, request: Request): Promise<an
 	return await syncSourceService.updateCache();
 }
 
-export async function internal_crawler_source(client: Client, request: Request): Promise<any> 
+export async function internal_crawler_source(client: Client, request: Request): Promise<SearchData<AddonData>> 
 {
-	throw new Error("Not implemented");
+	const dal = new docDbDal(ServerHelper.createPapiClient(client, PfsAddonUUID, client.AddonSecretKey));
+	const crawlingSourceService = new CrawlingSourceService(dal, request);
+
+	return await crawlingSourceService.getNextPage();
 }
 
 export async function internal_crawler_target(client: Client, request: Request): Promise<any> 
