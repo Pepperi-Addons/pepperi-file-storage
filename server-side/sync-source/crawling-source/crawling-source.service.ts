c80a6e4c7ca49df18ed9b13187a932702ce87fc1
Saar Guttman <saar.g@pepperi.com>
2024-01-04 11:12:06 +0200

    Add Crawler Source logging

--- server-side/sync-source/crawling-source/crawling-source.service.ts
+++ server-side/sync-source/crawling-source/crawling-source.service.ts
@@ -21,19 +21,28 @@ export class CrawlingSourceService
      */
     public async getNextPage(): Promise<SearchData<AddonData>>
     {
+        console.log("Getting the next page...");
+
         const pageKey: CrawlerPageKey = this.parsePageKey();
+        console.log("Parsed page key:", pageKey);
+
         const searchBody: SearchBody = {
             PageKey: pageKey.SpecificSchemaPageKey,
             Fields: ["Key", "Hidden", "ModificationDateTime"],
             IncludeDeleted: true,
-        }
+        };
 
         const schemaName = this.request.body.SchemaNames[pageKey.SchemaIndex];
+        console.log("Fetching data from schema:", schemaName);
+
         const result = await this.docDbDal.searchDataInTable(schemaName, searchBody);
+        console.log("Data fetched.");
 
         // Since we can't pass to the crawl target the actual schema from which the data was taken,
         // a transformation of the objects is required.
         this.formatResult(result, schemaName, pageKey);
+
+        console.log("Next page retrieved.");
 
         return result;
     }
@@ -46,19 +55,23 @@ export class CrawlingSourceService
      */
     protected formatResult(result: SearchData<AddonData>, schemaName: string, currentPageKey: CrawlerPageKey): void
     {
+        console.log("Formatting result...");
+
         this.setOriginSchemaOnObjects(result, schemaName);
         this.setNextPageKey(result, currentPageKey);
+
+        console.log("Result formatted.");
     }
 
-    /**
-     * Set the origin schema name on the objects in the result.
-     * @param {SearchData<AddonData>} result - The result to set the origin schema name on.
-     * @param {string} schemaName - The name of the schema from which the data was taken.
-     */
-    protected setOriginSchemaOnObjects(result: SearchData<AddonData>, schemaName: string) {
+    protected setOriginSchemaOnObjects(result: SearchData<AddonData>, schemaName: string)
+    {
+        console.log("Setting origin schema on objects...");
+
         result.Objects.forEach(object => {
             object["ObjectSourceSchema"] = schemaName;
         });
+
+        console.log("Origin schema set on objects.");
     }
 
     /**
@@ -68,17 +81,18 @@ export class CrawlingSourceService
      */
     protected setNextPageKey(result: SearchData<AddonData>, currentCrawlerPageKey: CrawlerPageKey): void
     {
+        console.log("Setting next page key...");
+
         const nextPageKey: CrawlerPageKey = {
             SchemaIndex: currentCrawlerPageKey.SchemaIndex,
             SpecificSchemaPageKey: "",
         };
 
         // If there is a next page key, on the same schema, use it.
-        if(result.NextPageKey)
+        if (result.NextPageKey)
         {
             nextPageKey.SpecificSchemaPageKey = result.NextPageKey;
             result.NextPageKey = JSON.stringify(nextPageKey);
-
         }
         // If there is no next page key, and there are more schemas to crawl, use the next schema.
         else if(currentCrawlerPageKey.SchemaIndex + 1 < this.request.body.SchemaNames.length)
@@ -90,8 +104,10 @@ export class CrawlingSourceService
         // to indicate that there are no more pages.
         else
         {
-            result.NextPageKey = ""
+            result.NextPageKey = "";
         }
+
+        console.log("Next page key set:", JSON.stringify(result.NextPageKey));
     }
 
     /**
@@ -100,21 +116,25 @@ export class CrawlingSourceService
      */
     protected parsePageKey(): CrawlerPageKey
     {
+        console.log("Parsing page key...");
+
         const result: CrawlerPageKey = { SchemaIndex: 0, SpecificSchemaPageKey: "" };
 
         const passedPageKey: string = this.request.body.PageKey;
-        if(passedPageKey)
+        if (passedPageKey)
         {
             const parsedPageKey: CrawlerPageKey = JSON.parse(passedPageKey);
 
             result.SchemaIndex = parsedPageKey.SchemaIndex;
             result.SpecificSchemaPageKey = parsedPageKey.SpecificSchemaPageKey ?? "";
 
-            if(!result.SchemaIndex)
+            if (!result.SchemaIndex)
             {
                 throw new Error("Error parsing PageKey: SchemaIndex is required");
             }
         }
+
+        console.log("Page key parsed:", JSON.stringify(result));
 
         return result;
     }
